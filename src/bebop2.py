66acf8a6c9929467709e699d082d6e957bf0ceac
Arsanios Mickael <synosx@gmail.com>
2020-12-09 09:51:39 +0100

    add pid and safety measures

--- src/bebop2.py
+++ src/bebop2.py
@@ -2,10 +2,47 @@
 
 import rospy
 import tf
+import math
 from math import atan2, sqrt, cos, sin, pi
 from std_msgs.msg import Empty
 from geometry_msgs.msg import Twist, TransformStamped, Vector3
 from bebop_msgs.msg import Ardrone3PilotingStateFlyingStateChanged
+
+WAYPOINTS = [
+    (0, 0),
+    (1.5, 0),
+    (1.5, 1.5),
+    (0, 1.5),
+    (0, 0)
+]
+
+
+class PID(object):
+    """PID class returns the pid error
+    """
+
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.prevTime = None
+        self.prevError = 0
+        self.cumError = 0
+
+    def calculate_pid(self, error):
+        cur_time = rospy.get_time()
+        if self.prevTime == None:
+            self.prevTime = cur_time
+            self.prevError = error
+            return self.kp * error
+        dt = cur_time - self.prevTime
+        self.prevTime = cur_time
+        self.cumError += error * dt
+        pid = self.kp * error + self.kd * \
+            (error - self.prevError) / dt + \
+            min(max(-0.2, self.ki * self.cumError), 0.2)
+        self.prevError = error
+        return pid
 
 
 class Bebop_functions():
@@ -15,97 +52,120 @@ class Bebop_functions():
 
         # Publisher which will publish to the topic '/bebop/takeoff'.
         self.takeoff_publisher = rospy.Publisher('/bebop/takeoff',
-                                                  Empty, queue_size=10)
-        
+                                                 Empty, queue_size=10)
+
         # Publisher which will publish to the topic '/bebop/land'.
         self.landing_publisher = rospy.Publisher('/bebop/land',
-                                                  Empty, queue_size=10)
+                                                 Empty, queue_size=10)
 
         # Publisher which will publish to the topic '/bebop/cmd_vel'.
         self.cmdvel_publisher = rospy.Publisher('/bebop/cmd_vel',
-                                                  Twist, queue_size=10)
+                                                Twist, queue_size=10)
 
         # A subscriber to the topic '/bebop/states/ardrone3/PilotingState/FlyingStateChanged'. self.update_state is called
         # when a message of type Ardrone3PilotingStateFlyingStateChanged is received.
         self.pilotingstate_subscriber = rospy.Subscriber('/bebop/states/ardrone3/PilotingState/FlyingStateChanged',
-                                                Ardrone3PilotingStateFlyingStateChanged, self.update_state)
+                                                         Ardrone3PilotingStateFlyingStateChanged, self.update_state)
         # Subscriber to the vicon transform node to read the position and quaternions of the bebop2
-        self.bebopcoord_subscriber = rospy.Subscriber('/vicon/yolanda/yolanda', TransformStamped, self.bebop_pose)
+        self.bebopcoord_subscriber = rospy.Subscriber(
+            '/vicon/yolanda/yolanda', TransformStamped, self.bebop_pose)
 
-        self.state=None
-        self.yaw=0
-        self.bebopose=Vector3()
-        self.rate = rospy.Rate(5)  # TransformStamped message update rate limited to 5 Hz, which limits to 5 Hz the subscribing rate.
+        self.state = None
+        self.yaw = 0
+        self.bebopose = Vector3()
 
-    def takeoff(self): # Take off command function
+        self.controlX = PID(0.08, 0.001, 0.05)
+        self.controlY = PID(0.08, 0.001, 0.05)
+        self.controlZ = PID(0.08, 0.001, 0.05)
+
+        self.vel_lim = 2.0
+        self.finished = False
+        self.waypoint = WAYPOINTS.pop(0)
+        # TransformStamped message update rate limited to 5 Hz, which limits to 5 Hz the subscribing rate.
+        self.rate = rospy.Rate(5)
+
+    def takeoff(self):  # Take off command function
         while self.state != 2:
             hello_str = "Takeoff command has been sent %s" % rospy.get_time()
             rospy.loginfo(hello_str)
             self.takeoff_publisher.publish(Empty())
             self.rate.sleep()
-
         print("Takeoff is done!")
-    
-    def land (self): # Land command function
+
+    def land(self):  # Land command function
         while self.state != 0:
             hello_st = "Land command has been sent %s" % rospy.get_time()
             rospy.loginfo(hello_st)
             self.landing_publisher.publish(Empty())
             self.rate.sleep()
-        
+
         print("Landing completed succesfully!")
-    
-    def move(self): # Move function with a P controller.
 
-        krho = 0.02 # P controller parameters setup
-        kalpha = 0.2
-        kbeta = 0.01
+    def move(self):  # Move function with a P controller.
+
         twist = Twist()
-
-        x = 0
-        y = 0
+        # destination fixed z
         z = 1.0
 
-        while not rospy.is_shutdown():
+        while not rospy.is_shutdown() and not self.finished:
 
-            delta_x = x - self.bebopose.x # position error
-            delta_y = y - self.bebopose.y
+            delta_x = self.waypoint[0] - self.bebopose.x  # position error
+            delta_y = self.waypoint[1] - self.bebopose.y
             delta_z = z - self.bebopose.z
 
-            speed_x = delta_x*krho # calculated speeds for the P control
-            speed_y = delta_y*krho
-            speed_z = delta_z*krho
-            
+            rho = sqrt(delta_x*delta_x + delta_y*delta_y +
+                       delta_z*delta_z)  # error to the goal
+            if rho < 0.2:
+                if len(WAYPOINTS) != 0:
+                    self.waypoint = WAYPOINTS.pop(0)
+                else:
+                    self.finished = True
+            error_x = math.cos(self.yaw) * delta_x + \
+                math.sin(self.yaw) * delta_y
+            error_y = -math.sin(self.yaw) * delta_x + \
+                math.cos(self.yaw) * delta_y
 
-            rho = sqrt(delta_x*delta_x + delta_y*delta_y + delta_z*delta_z) # error to the goal
             rospy.loginfo("rho: {:.2f}, yaw: {:.2f}".format(rho, self.yaw))
 
-            twist.linear.x = speed_x*cos(self.yaw) + speed_y*sin(self.yaw) # robot's body frame convertion for x and y linear velocities
-            twist.linear.y = - speed_x*sin(self.yaw) + speed_y*cos(self.yaw)
-            twist.linear.z = speed_z
-            
-            self.cmdvel_publisher.publish(twist) # publishing of angular rates and linear velocities
-            self.rate.sleep()
+            # robot's body frame convertion for x and y linear velocities
+            twist.linear.x = max(min(
+                self.controlX.calculate_pid(error_x), self.vel_lim), -self.vel_lim)
+            twist.linear.x = max(min(
+                self.controlY.calculate_pid(error_y), self.vel_lim), -self.vel_lim)
+            twist.linear.x = max(min(
+                self.controlZ.calculate_pid(delta_z), self.vel_lim), -self.vel_lim)
 
+            # publishing of angular rates and linear velocities
+            self.cmdvel_publisher.publish(twist)
+            self.rate.sleep()
+        if self.finished:
+            self.land()
         print("Move is done!")
 
-    def update_state(self, message): # /bebop/states... callback subscriber to read the state of the parrot
+    # /bebop/states... callback subscriber to read the state of the parrot
+    def update_state(self, message):
 
         self.state = message.state
-    
-    def bebop_pose(self, msg): # /vicon/yolanda/yolanda callback subscriber to know the position and the yaw angle of the robot 
+
+    def safety_check(self):
+        if self.bebopose.x > 3.0 or self.bebopose.x > 3.0 or self.bebopose.z > 2.0:
+            self.finished = True
+
+    # /vicon/yolanda/yolanda callback subscriber to know the position and the yaw angle of the robot
+    def bebop_pose(self, msg):
 
         self.bebopose = msg.transform.translation
-
         quat = msg.transform.rotation
-        (roll, pitch, self.yaw) = tf.transformations.euler_from_quaternion((quat.x, quat.y, quat.z, quat.w))
+        (roll, pitch, self.yaw) = tf.transformations.euler_from_quaternion(
+            (quat.x, quat.y, quat.z, quat.w))
+        self.safety_check()
 
 
 if __name__ == '__main__':
     try:
         node = Bebop_functions()
-        #node.takeoff()
+        node.takeoff()
         node.move()
-        #node.land()
-    except rospy.ROSInterruptException:
-        pass
+        node.land()
+    except rospy.ROSInterruptException as e:
+        print "Exception: {0}".format(str(e))
